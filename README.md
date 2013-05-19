81699f4f7a33bce55f939906b066079e89f4066a
Bradley Skaggs <bskaggs@cs.umd.edu>
2013-05-19 14:05:02 -0400

    only backup if not already correct symlink

---
+++ README.md
@@ -0,0 +1,8 @@
+My dotfiles
+===========
+
+Installation
+------------
+
+External dependencies are included via git submodules.  So make sure to clone with
+    git clone --recursive git@github.com:bskaggs/dotfiles.git
--- install.sh
+++ install.sh
@@ -3,12 +3,18 @@ shopt -s extglob
 dir=$( cd $(dirname $0) ; pwd -P )
 backup=~/olddotfiles.$(date +"%Y%m%d%H%M%S")
 
-for src in ${dir}/!($(basename $0)|README); do
+for src in ${dir}/!($(basename $0)|README.md); do
 	file=~/.$(basename "$src")
-		
-	if [ -e "$file" ]; then
-		mkdir -p "$backup"
-		mv "$file" "$backup"
-	fi
-	ln -s "$src" "$file"
+	
+  if [ -L "$file" -a $(readlink "$file") == "$src" ]; then
+    echo "Symlink $file already exists and is correct"
+  else
+    if [ -e "$file" ]; then
+      echo "Backing up $file"
+      mkdir -p "$backup"
+      mv "$file" "$backup"
+    fi
+    echo "Symlinking $file to $src"
+    ln -s "$src" "$file"
+  fi
 done
