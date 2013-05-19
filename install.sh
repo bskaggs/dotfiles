27698c45c246eae998ed249ffb3e228379a32dc1
Bradley Skaggs <bskaggs@cs.umd.edu>
2013-05-18 21:21:31 -0400

    initial dotfiles attempt

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+shopt -s extglob
+dir=$( cd $(dirname $0) ; pwd -P )
+backup=~/olddotfiles.$(date +"%Y%m%d%H%M%S")
+
+for src in ${dir}/!($(basename $0)); do
+	file=~/.$(basename "$src")
+		
+	if [ -e "$file" ]; then
+		mkdir -p "$backup"
+		mv "$file" "$backup"
+	fi
+	ln -s "$src" "$file"
+done
